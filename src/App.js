d0057fa18ab2a49fbf5e48ddfd3b96de6feeef24
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-02-10 06:06:46 -0800

    directory and categories styled and added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import Directory from './components/directory/directory.component';
 
 function App() {
+  const categories = [
+    { id: 1, title: 'hats', imageUrl: 'https://i.ibb.co/cvpntL1/hats.png', },
+    { id: 2, title: 'tops', imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png', },
+    { id: 3, title: 'bottoms', imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png', },
+    { id: 4, title: 'overalls', imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',},
+    { id: 5, title: 'dresses', imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',},
+    { id: 5, title: 'shoes', imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',},
+    { id: 6, title: 'mens', imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',},
+    { id: 7, title: 'womens', imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',},
+  ];
+
   return (
-    <div className="App">
-      <div className="bg-blue-900 h-screen"></div>
+    <div className='outer-container'>
+      <Directory categories={categories} />
     </div>
+    
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    fontFamily: {
+      'noto-sans-syloti-nagri': ['Noto Sans Syloti Nagri', 'sans serif'],
+    }
   },
   plugins: [],
 }
