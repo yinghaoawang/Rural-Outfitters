e7fa3d3b904fb43c6322e5134cf5b8e87e2b97fd
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-02-21 18:05:26 -0800

    basename removed for netlify

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { FilterProvider } from './contexts/filter.context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/rural-outfitters'>
+    <BrowserRouter>
       <UserProvider>
         <CartProvider>
           <FilterProvider>
