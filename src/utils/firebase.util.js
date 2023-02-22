3bc17eabc3fc541d5c12a9a207fd8cb7267fc6f6
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-02-21 19:10:42 -0800

    env vars properly named

--- src/utils/firebase.util.js
+++ src/utils/firebase.util.js
@@ -1,10 +1,19 @@
 import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from './config.js';
 import {
     getAuth, onAuthStateChanged, signOut,
     signInWithRedirect, signInWithPopup, signInWithEmailAndPassword,
     GoogleAuthProvider, createUserWithEmailAndPassword,  } from 'firebase/auth'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FB_API_KEY,
+    authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FB_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FB_APP_ID,
+    measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
+};
 
 const firebaseApp = initializeApp(firebaseConfig);
 const googleProvider = new GoogleAuthProvider();
